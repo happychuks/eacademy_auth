1507a3233e55e25f38a2390483fcd435ffd264cf
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-11-03 19:10:28 +0100

    feat: user authentication url paths

---
+++ authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import SignUpView, LoginView, UserDetailView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', UserDetailView.as_view(), name='user_detail'),
+]
